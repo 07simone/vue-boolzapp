820cff7c592064a62a7e66c75eb24260afda6429
simone andreozzi <simoneandreozzi8@gmail.com>
2022-03-22 13:04:50 +0100

    modifiche-struttura-inserimento-lista

--- js/main.js
+++ js/main.js
@@ -181,15 +181,15 @@ const app = new Vue(
 )
 
 
-
- const avatar = '_1';     // img_2/avatar_1.jpg
+/* 
+const avatar = '_1';     
 
 function imgAvatar(numeroConTrattinoBasso){
     
 return "img_2/avatar" + numeroConTrattinoBasso + ".jpg"
 
 }
-console.log(imgAvatar(avatar))
+console.log(imgAvatar(avatar)) */
 
 
 
